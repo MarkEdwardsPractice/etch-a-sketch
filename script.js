43f14e204fa115a14ebcff1c17a07103425fda47
MarkEdwardsPractice <markedwardspractice@gmail.com>
2024-05-17 13:15:08 +0900

    Made a grid that keeps colored values

--- script.js
+++ script.js
@@ -10,57 +10,71 @@ container2.style.display = "flex";
 container3.style.display = "flex";
 container4.style.display = "flex";
 
-body.appendChild(container1);
-body.appendChild(container2);
-body.appendChild(container3);
-body.appendChild(container4);
 
-for(let i = 0; i < 4; i++)
+bigContainer.appendChild(container1);
+bigContainer.appendChild(container2);
+bigContainer.appendChild(container3);
+bigContainer.appendChild(container4);
+
+bigCWidth = document.getElementById("bigContainer").clientWidth;
+bigCHeight = document.getElementById("bigContainer").clientHeight;
+
+const NUMBER_OF_SQUARES = 16;
+divWidth = bigCWidth / NUMBER_OF_SQUARES;
+divHeight = bigCHeight / NUMBER_OF_SQUARES;
+for(let i = 0; i < NUMBER_OF_SQUARES; i++)
 {
     
     const div = document.createElement("div");
-    div.style.width = "10px";
-    div.style.height = "10px";
+    div.style.width = `${divWidth}px`;
+    div.style.height = `${divHeight}px`;
     div.style.border = "2px solid black";
     div.addEventListener("mouseover", () => {
         div.style.backgroundColor = "red";
     });
-    div.addEventListener("mouseout", () => {
-        div.style.backgroundColor = "";
-    });
+
     container1.appendChild(div);
     
 }
 
-for(let i = 0; i < 4; i++)
+for(let i = 0; i < NUMBER_OF_SQUARES; i++)
 {
         
         const div = document.createElement("div");
-        div.style.width = "10px";
-        div.style.height = "10px";
+        div.style.width = `${divWidth}px`;
+        div.style.height = `${divHeight}px`;
         div.style.border = "2px solid black";
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "red";
+        });
         container2.appendChild(div);
         
 }
 
-for(let i = 0; i < 4; i++)
+for(let i = 0; i < NUMBER_OF_SQUARES; i++)
 {
             
     const div = document.createElement("div");
-    div.style.width = "10px";
-    div.style.height = "10px";
+    div.style.width = `${divWidth}px`;
+    div.style.height = `${divHeight}px`;
     div.style.border = "2px solid black";
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "red";
+    });
     container3.appendChild(div);
             
 }
 
-for(let i = 0; i < 4; i++)
+for(let i = 0; i < NUMBER_OF_SQUARES; i++)
 {
                 
     const div = document.createElement("div");
-    div.style.width = "10px";
-    div.style.height = "10px";
+    div.style.width = `${divWidth}px`;
+    div.style.height = `${divHeight}px`;
     div.style.border = "2px solid black";
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "red";
+    });
     container4.appendChild(div);
                 
 }
