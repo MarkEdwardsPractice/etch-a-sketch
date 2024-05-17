37bb18b07053f546aba2b418f532e17adde7aefe
MarkEdwardsPractice <markedwardspractice@gmail.com>
2024-05-17 14:17:48 +0900

    Change row containers into an array of containers to be added to, change individual row creation into function

--- script.js
+++ script.js
@@ -1,80 +1,46 @@
 const body = document.querySelector("body");
 
-const container1 = document.createElement("div");
-const container2 = document.createElement("div");
-const container3 = document.createElement("div");
-const container4 = document.createElement("div");
+const NUMBER_OF_SQUARES = 16;
 
-container1.style.display = "flex";
-container2.style.display = "flex";
-container3.style.display = "flex";
-container4.style.display = "flex";
+const containers = [];
+
+for(let i = 0; i < NUMBER_OF_SQUARES; i++)
+{
+    containers.push(document.createElement("div"));
+    containers[i].style.display = "flex";
+    bigContainer.appendChild(containers[i]);
+}
 
 
-bigContainer.appendChild(container1);
-bigContainer.appendChild(container2);
-bigContainer.appendChild(container3);
-bigContainer.appendChild(container4);
+
+
 
 bigCWidth = document.getElementById("bigContainer").clientWidth;
 bigCHeight = document.getElementById("bigContainer").clientHeight;
 
-const NUMBER_OF_SQUARES = 16;
+
 divWidth = bigCWidth / NUMBER_OF_SQUARES;
 divHeight = bigCHeight / NUMBER_OF_SQUARES;
-for(let i = 0; i < NUMBER_OF_SQUARES; i++)
-{
-    
-    const div = document.createElement("div");
-    div.style.width = `${divWidth}px`;
-    div.style.height = `${divHeight}px`;
-    div.style.border = "2px solid black";
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "red";
-    });
 
-    container1.appendChild(div);
-    
-}
-
-for(let i = 0; i < NUMBER_OF_SQUARES; i++)
+function createRow()
 {
-        
-        const div = document.createElement("div");
-        div.style.width = `${divWidth}px`;
-        div.style.height = `${divHeight}px`;
-        div.style.border = "2px solid black";
-        div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "red";
-        });
-        container2.appendChild(div);
-        
-}
-
-for(let i = 0; i < NUMBER_OF_SQUARES; i++)
-{
+    for(let i = 0; i < NUMBER_OF_SQUARES; i++)
+        {
             
-    const div = document.createElement("div");
-    div.style.width = `${divWidth}px`;
-    div.style.height = `${divHeight}px`;
-    div.style.border = "2px solid black";
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "red";
-    });
-    container3.appendChild(div);
+            const div = document.createElement("div");
+            div.style.width = `${divWidth}px`;
+            div.style.height = `${divHeight}px`;
+            div.style.border = "2px solid black";
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = "red";
+            });
+        
+            containers[i].appendChild(div);
             
+        }
 }
 
 for(let i = 0; i < NUMBER_OF_SQUARES; i++)
 {
-                
-    const div = document.createElement("div");
-    div.style.width = `${divWidth}px`;
-    div.style.height = `${divHeight}px`;
-    div.style.border = "2px solid black";
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "red";
-    });
-    container4.appendChild(div);
-                
+    createRow();
 }
